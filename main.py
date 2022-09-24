221386695bde534a31c09197a0a5b1fbe62aa086
akuby21 <akuby21@gmail.com>
2022-09-24 14:33:20 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from SQLPkg.CRUD import Landmark_CRUD
 import telegram
 import psycopg2
+import json
+import os
+import glob
 #import googlemaps
 import numpy as np
 import math
@@ -9,21 +12,21 @@ from telegram import Update, Bot
 from telegram.ext import Updater, CallbackContext, CommandHandler
 from telegram.ext import MessageHandler, Filters
 import time
-USERNUMBER = 10
 
 gApi = ""
 token = ''
-limit_landmark_distance = 0.01
-can_go_landmark = [[0 for col in range(USERNUMBER)] for row in range(99)]
-new_can_go = [[0 for col in range(USERNUMBER)] for row in range(99)]
+limit_landmark_distance = 0.1
+can_go_landmark = []
+new_can_go = []
 bot = telegram.Bot(token)
 SCHEMA = 'landmark'
 TABLE = 'place'
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
-
+path = "./User_Data/*.json"
+[os.remove(f) for f in glob.glob(path)]
 updater.start_polling()
-
+crud = Landmark_CRUD('azureuser')
 def handler(update: Update, context:CallbackContext):
     id = update.message.chat_id
     user_text = update.message.text
@@ -37,7 +40,6 @@ def cal_distance(start,end):
 
 def location(update:Update, context:CallbackContext):
     global limit_landmark_distance
-    print(f'>> {update}')
     message = None
     if update.edited_message:
         message = update.edited_message
@@ -47,38 +49,41 @@ def location(update:Update, context:CallbackContext):
     current_pos = (message.location.latitude, message.location.longitude)
     id = message.chat.id
     print(f'{id} : {current_pos}')
-    pid = id % 50
-    coordinates = [[35.867576, 128.6245084],[35.8605323, 128.5599617],[37.5384272, 126.9654442],[35.867808, 128.624797],[35,128],[35.8674923, 128.6242198], [35.8605201, 128.5598575]]
-    
+    data = crud.read(SCHEMA,TABLE,'id','coordinate')
     new_can_go = [] # 랜드마크 ID
 
     # 현재 위치에서 갈 수 있는 랜드마크를 new_can_go에 저장
-    for i in coordinates:
-        if cal_distance(current_pos, i) < limit_landmark_distance:
-            new_can_go[pid].append(i)
+    for i in data:
+        if cal_distance(current_pos, i[1]) < limit_landmark_distance:
+            new_can_go.append(i[0])
     
     #can_go_landmark json파일 불러오기
     # ...
 
+    try:
+        with open(f"./User_Data/{id}.json",'r') as outfile:
+            userLandData = json.load(outfile)['Landmark']
+    except:
+        userLandData = []
+    print(new_can_go,userLandData,type(userLandData))	
     # 새로 갈 수 있는 랜드마크와 기존에 갈 수 있는 랜드마크가 다르고
-    if set(new_can_go) != set(can_go_landmark): # set 비교
+    if set(new_can_go) != set(userLandData): # set 비교
         # 갈 곳 없으면 "NO WHERE TO GO", 갈 곳 있으면 새로운 장소
         if set(new_can_go) == set(): #비었으면
-            bot.send_message(chat_id=id, text="NO WHERE TO GO")
+            bot.send_message(chat_id=id, text="NOWHERE TO GO")
         else:
-            bot.send_message(chat_id=id, text=new_can_go[pid])
+            for landmarks in new_can_go:
+                newText = curd.execute(f"SELECT * FROM WHERE id = {landmarks[0]}",True)
+                bot.send_message(chat_id=id, text=newText)
     
     # 기존 갈 수 있는 장소 업데이트
     # json write
-    can_go_landmark = copy.deepcopy(new_can_go)
+    with open(f"./User_Data/{id}.json",'w') as outfile:
+        json.dump({'id':id,'Landmark':new_can_go},outfile)
     
     
-crud = Landmark_CRUD('azureuser')
-data = crud.read(SCHEMA,TABLE,'id','coordinate','name')
-print(data[-1])
 echo_handler = MessageHandler(Filters.text, handler)
 location_handler = MessageHandler(Filters.location, location)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(location_handler)
-
 
